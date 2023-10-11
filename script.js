946bde27b9badb738e664fe16b773bf78d800fce
kaustin <krystalaustin99@gmail.com>
2023-10-10 21:02:57 -0500

    not dynamic js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const container = document.getElementById('imageContainer');
 
-// Fetch the list of image files dynamically
-fetch('images/')
-    .then(response => response.text())
-    .then(text => {
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(text, 'text/html');
-        const links = Array.from(htmlDoc.querySelectorAll('a'));
-        const imageFiles = links
-            .filter(link => link.href.match(/\.(jpe?g|png|gif)$/i))
-            .map(link => link.href);
+const imageFiles = [
+    'images/HebSpirit01.jpg',
+    'images/HebSpirit02.jpg',
+    'images/HebSpirit03.jpg',
+    'images/HebSpirit04.jpg',
+    'images/HebSpirit05.jpg',
+    'images/HebSpirit06.jpg',
+    'images/HebSpirit07.jpg'
+];
 
-        // Loop through the image files and create img elements
-        imageFiles.forEach((file, index) => {
-            const img = document.createElement('img');
-            img.src = file;
-            img.alt = `H-E-B Spirit Award ${index + 1}`;
-            img.classList.add('image');
-            container.appendChild(img);
-        });
-    })
-    .catch(error => console.error('Error fetching images:', error));
+// Loop through the image files and create img elements
+imageFiles.forEach((file, index) => {
+    const img = document.createElement('img');
+    img.src = file;
+    img.alt = `H-E-B Spirit Award ${index + 1}`;
+    img.classList.add('image');
+    container.appendChild(img);
+});
